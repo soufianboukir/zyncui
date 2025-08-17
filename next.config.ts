1213d63f51936d1995d32665d8c99f9f59bc69c1
sofyan <soufianeboukir0@gmail.com>
2025-08-17 15:24:35 +0100

    run prettier to avoid CI failing

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["randomuser.me", "images.unsplash.com"],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
